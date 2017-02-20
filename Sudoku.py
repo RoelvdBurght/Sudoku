3e66c83f15ecf97608f0cf43adeb9584b03a39f1
Roel <rgburght@gmail.com>
2017-02-20 19:44:01 +0100

    versie 20-02 #1 sudoku is nu een int array ipv een string array

--- Sudoku.py
+++ Sudoku.py
@@ -18,22 +18,23 @@ sudoku = [[0 for x in range(w)] for y in range(h)]
 for x in range(len(sudoku)):
     for y in range(len(sudoku)):
         str = horLijnDict[x]
-        sudoku[x][y] = str[y]
+        sudoku[x][y] = int(str[y])
 
 
-#returned True als een rij geschikt is voor checkNumber
+#returned True als een rij geschikt is voor het nummer checkNumber
 def rowCheck(rowIndex, checkNumber):
     row = sudoku[rowIndex]
     for i in range(8):
-        number = int(row[i])
+        number = row[i]
         if checkNumber == number:
-            return True
-    return False
+            return False
+    return True
 
 #Returned True als een kolom geschikt is voor checkNumber
 def collCheck(collIndex, checkNumber):
     for i in range(8):
         number = int(sudoku[i][collIndex])
+
         if number == checkNumber:
             return False
     return True
@@ -41,8 +42,9 @@ def collCheck(collIndex, checkNumber):
 #maakt een 3x3 array van arrays uit de grote sudoku
 #met hor & vert als linker bovenhoek
 #returnt 3x3 blokje als output
-
-def makeSq(vert, hor):
+def makeSq(*array):
+    hor = array[0]
+    vert = array[1]
     tempSq = [[0 for p in range(3)]for q in range(3)]
     print (tempSq)
     for i in range(3):
@@ -64,10 +66,11 @@ def spotCheck(spotIndex, checkNumber):
 
 x = makeSq(3,3)
 print(x)
-def checkSq(array[][]):
+"""
+def checkSq(*array):
     spotCounter = -1
     for i in range(2):
         for j in range(2):
             spotCounter += 1
             if array[i][j] == 0:
-                
+"""
