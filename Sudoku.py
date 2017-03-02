e8ddab59cb9f478c0f55587dae751177e93fc9d0
Bart <hbpvanlaatum123@gmail.com>
2017-03-02 19:58:29 +0100

    dat ding werkt gewoooon joh

--- Sudoku.py
+++ Sudoku.py
@@ -3,7 +3,7 @@ w = 9
 h = 9
 
 # opent file en assignt de lijnen in een dictionary
-sudokuFile = open("puzzle1.sudoku", "r")
+sudokuFile = open("puzzle4.sudoku", "r")
 horLijnDict = {}
 x = 0       #als iemand hier een elegantere oplossing voor heeft: graag
 for line in sudokuFile:
@@ -68,7 +68,7 @@ def sqCheck(row, col, checkNumber):
 # Kijkt of een specifieke plek in de sudoku geschikt is voor een nummer
 # Returned True als de plek geschikt is
 def spotCheck(row, col, checkNumber):
-    if sqCheck(row, col, checkNumber) == True and collCheck(col, checkNumber) == True:
+    if sqCheck(row, col, checkNumber) == True and collCheck(col, checkNumber) == True and rowCheck(row, checkNumber) == True:
         return True
     return False
 
@@ -92,7 +92,6 @@ def isSudokuFilled(sudoku):
                 return False
     return True
 
-
 def solveRow(row):
     tempList = usableInts(row)
     for num in tempList:
@@ -115,35 +114,37 @@ def placeNumber(row, num):
             if sudoku[row][j] == 0 and spotCheck(row, j, num) == True:
                 sudoku[row][j] = num
 
-def solveSudoku(sudoku):
-    while isSudokuFilled(sudoku) == False:
-        for i in range(9):
-            solveRow(i)
+def nextSpot(spot):
+    if spot[1] == 8:
+        spot[1] = 0
+        spot[0] += 1
+    else:
+        spot[1] += 1
+    return spot
 
-'''solveRow(0)
-solveRow(1)
-solveRow(2)
-solveRow(3)
-solveRow(4)
-solveRow(5)
-solveRow(6)
-solveRow(7)
-solveRow(8)
-'''
-solveSudoku(sudoku)
-print(sudoku)
-""""
-def knalleuh(sudoku):
-    for i in range(row):
-        tempList = usableInts(row)
-        for j in range(col):
-            if sudoku[row][col] == 0
-                counter = 0
-                for len(tempList):
-                    if(collCheck(col, tempList[counter]) == True && sqCheck(row, col, tempList[counter]) == True)
-                    als beide bovenstaande evaluaties true zijn
-                        onthoud plek en getal dat daar kan
-                als er 2 getallen kunnen -> volgend hokje
-                als er 1 getal kan -> vul dat getal in
-    check of sudoku gevuld is
-"""
+def printsudoku():
+    for i in range(9):
+        print(sudoku[i])
+
+def findEmpty(sudoku):
+    for row in range(9):
+        for col in range(9):
+            if sudoku[row][col] == 0:
+                return row, col
+
+
+def solveSudoku(sudoku):
+    if isSudokuFilled(sudoku):
+        return True
+    row, col = findEmpty(sudoku)
+    list = range(1,10)
+    for num in list:
+        if spotCheck(row, col, num):
+            sudoku[row][col] = num
+            if solveSudoku(sudoku):
+                return True
+    sudoku[row][col] = 0
+    return False
+
+print(solveSudoku(sudoku))
+printsudoku()
